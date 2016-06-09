c044e9e4638b0da61bd23d6e2d98e3d899d0cfe4
Tonny Boesenbaek <tonnyb@gmail.com>
2016-06-09 00:32:00 -0400

    Added 5 sample mains and sample-output files, and assignment instruction files

---
+++ SmartArray/sample-main5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "SmartArray.h"
+
+// Boundary check to make sure your difficultyRating() and hoursSpent()
+// functions are implemented correctly.
+
+int main(void)
+{
+	if (difficultyRating() < 1.0 || difficultyRating() > 5.0)
+		printf("difficultyRating() test: **fail**\n");
+	else
+		printf("difficultyRating() test: PASS\n");
+
+	if (hoursSpent() < 0.0)
+		printf("hoursSpent() test: **fail**\n");
+	else
+		printf("hoursSpent() test: PASS\n");
+
+	system("pause");
+	return 0;
+}
